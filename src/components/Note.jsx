4713e6522201050644bb5d80aa0b62aa59053dbf
Tim Guo <gtm1224@berkeley.edu>
2022-03-15 19:04:14 -0700

    finished all functionalities

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 
 function Note(props){
+    function handleChange(){
+        props.onDelete(props.id)
+    }
     return <div className="note">
         <h1>{props.title}</h1>
         <p>{props.content}</p>
+        <button onClick={handleChange}>DELETE</button>
     </div>
 }
 
